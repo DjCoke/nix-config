413b7d16eb844928617292359b1690891055d9bb
DjCoke <8378780+DjCoke@users.noreply.github.com>
2024-11-24 21:56:40 +0100

    Found the bug, now restoring everything, so to pinpoint where the configVars.isMinimal is not correctly applied

--- vars/default.nix
+++ vars/default.nix
@@ -14,7 +14,7 @@
   persistFolder = "/persist";
 
   # System-specific settings (FIXME: Likely make options)
-  isMinimal = true; # Used to indicate nixos-installer build
+  isMinimal = false; # Used to indicate nixos-installer build
   isWork = false; # Used to indicate a host that uses work resources
   scaling = "1"; # Used to indicate what scaling to use. Floating point number
 }

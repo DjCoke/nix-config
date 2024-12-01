9462d9fa635ade0c0b773706d152eb653f38f693
DjCoke <8378780+DjCoke@users.noreply.github.com>
2024-12-01 20:26:07 +0100

    disabling the firewall

--- hosts/common/optional/k3s.nix
+++ hosts/common/optional/k3s.nix
@@ -38,14 +38,17 @@
       ;
   };
 
-  networking.firewall.allowedTCPPorts = [
-    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
-    2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
-    2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
-    10250 # k3s, metrics
-    10255 # k3s, metrics
-  ];
-  networking.firewall.allowedUDPPorts = [
-    8472 # k3s, flannel: required if using multi-node for inter-node networking
-  ];
+  # debugging my failures to setup my cluster
+  networking.firewall.enable = false;
+
+  # networking.firewall.allowedTCPPorts = [
+  #   6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+  #   2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+  #   2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+  #   10250 # k3s, metrics
+  #   10255 # k3s, metrics
+  # ];
+  # networking.firewall.allowedUDPPorts = [
+  #   8472 # k3s, flannel: required if using multi-node for inter-node networking
+  # ];
 }

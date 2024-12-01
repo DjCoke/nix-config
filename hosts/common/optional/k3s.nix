17de2c992ec969a028c571db7d7c0e21f66f0a4f
DjCoke <8378780+DjCoke@users.noreply.github.com>
2024-12-01 15:54:19 +0100

    k3s: slowly advancing

--- hosts/common/optional/k3s.nix
+++ hosts/common/optional/k3s.nix
@@ -1,28 +1,35 @@
-{ pkgs, lib, config, hostName, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  hostName,
+  ...
+}:
 {
   services.k3s = {
     enable = true;
     role = "server";
     tokenFile = "/var/lib/rancher/k3s/server/token";
-    extraFlags = toString (
-      [
-        "--write-kubeconfig-mode \"0644\""
-        "--cluster-init"
-        "--disable servicelb"
-        "--disable traefik"
-        "--disable local-storage"
-        #  "--flannel-iface=ens18"
-      ]
-      ++ (
-        if hostName == "k3s-01" then
-          [ ]
-        else
-          [
-            "--server-ip 192.168.1.145"
-          ]
-      )
+    extraFlags = toString ([
+      "--write-kubeconfig-mode \"0644\""
+      "--disable servicelb"
+      "--disable traefik"
+      "--disable local-storage"
+      #  "--flannel-iface=ens18"
+    ]
+    # ++ (
+    #   if hostName == "k3s-01" then
+    #     [ ]
+    #   else
+    #     [
+    #       "--server-ip 192.168.1.145"
+    #     ]
+    # )
     );
+    # first we check of this is master-server, if so, then ClusterInit
     clusterInit = (hostName == "k3s-01");
+    # id we know that clustInit = true; then this must be the master server, else server nodes
+    serverAddr = if hostName != "k3s-01" then "https://192.168.1.145:6443" else "";
   };
 
   services.openiscsi = {

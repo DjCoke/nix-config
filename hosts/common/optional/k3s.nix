726a4def979ccd691491473cf8277920268d5561
DjCoke <8378780+DjCoke@users.noreply.github.com>
2024-12-03 23:53:49 +0100

    rewrite of k3s to add more roles/k3s nodes

--- hosts/common/optional/k3s.nix
+++ hosts/common/optional/k3s.nix
@@ -6,36 +6,54 @@
   ...
 }:
 {
-  services.k3s = {
-    enable = true;
-    role = "server";
-    tokenFile = "/var/lib/rancher/k3s/server/token";
-    extraFlags = toString (
-      [
-        "--write-kubeconfig-mode \"0644\""
-        "--disable servicelb"
-        "--disable traefik"
-        "--disable local-storage"
-        "--flannel-iface=ens18"
+  services.k3s =
+    if
+      builtins.elem hostName [
+        "k3s-01"
+        "k3s-02"
+        "k3s-03"
       ]
-      ++ (
-        if hostName == "k3s-01" then
+    then
+      {
+        enable = true;
+        role = "server";
+        tokenFile = "/var/lib/rancher/k3s/server/token";
+        extraFlags = toString (
           [
-            "--node-ip=192.168.1.201"
-            "--tls-san=192.168.1.250" # voor de load balancer
-            "--tls-san=192.168.1.201" # k3s-01
-            "--tls-san=192.168.1.202" # k3s-02
-            "--tls-san=192.168.1.203" # k3s-03
+            "--write-kubeconfig-mode \"0644\""
+            "--disable servicelb"
+            "--disable traefik"
+            "--disable local-storage"
+            "--flannel-iface=ens18"
           ]
-        else
-          [ ]
-      )
-    );
-    # first we check of this is master-server, if so, then ClusterInit
-    clusterInit = (hostName == "k3s-01");
-    # id we know that clustInit = true; then this must be the master server, else server nodes
-    serverAddr = if hostName != "k3s-01" then "https://192.168.1.201:6443" else "";
-  };
+          ++ (
+            if hostName == "k3s-01" then
+              [
+                "--node-ip=192.168.1.201"
+                "--tls-san=192.168.1.250" # voor de load balancer
+                "--tls-san=192.168.1.201" # k3s-01
+                "--tls-san=192.168.1.202" # k3s-02
+                "--tls-san=192.168.1.203" # k3s-03
+              ]
+            else
+              [ ]
+          )
+        );
+        # first we check of this is master-server, if so, then ClusterInit
+        clusterInit = (hostName == "k3s-01");
+        # id we know that clustInit = true; then this must be the master server, else server nodes
+        serverAddr = if hostName != "k3s-01" then "https://192.168.1.201:6443" else "";
+      }
+    else
+      {
+        enable = false;
+        role = "worker";
+        server = "https://192.168.1.250:6443"; # Verbind met Kube-VIP
+        tokenFile = "/var/lib/rancher/k3s/server/token";
+        extraFlags = toString [
+          "--node-ip=192.168.1.2${builtins.substring 4 2 hostName}" # Automatisch IP bepalen
+        ];
+      };
 
   services.openiscsi = {
     enable = true;
